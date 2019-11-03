54c59b580e7ceff1742cbc7b5b66634ca98e36b7
Robert-GainesIII <gaines.robert.edgar@gmail.com>
2019-11-02 23:55:39 -0700

    update

---
+++ CS3B/lib/objectFiles/String_toUpperCase.s
@@ -0,0 +1,38 @@
+@*****************************************************************************
+@AUTHOR:		Robert Gaines
+@PROGRAM:   	+String_toUpper(string)     
+@[R1]:			points to address of string being converted
+@
+@RETURNS: 		R1 contains converted toupper string
+@
+@*****************************************************************************
+@===============================@
+toUpper:
+	push {r1, lr}
+	LDR R1, [R1]
+	mov r3, r1
+	bl String_length
+	mov r8, r0
+	
+firstLoop:
+	cmp r8, #0
+	beq endUpper
+	ldrb r7, [r3]
+	cmp r7, #97
+	blt nextChar
+	cmp r7, #122
+	bgt nextChar
+	sub r7, #32
+	strb r7, [r3],#1
+	sub r8, #1
+	b firstLoop
+nextChar:
+	add r3, #1
+	sub r8, #1
+	b firstLoop
+
+
+endUpper:
+	pop {lr}
+	bx lr
+@===============================@
