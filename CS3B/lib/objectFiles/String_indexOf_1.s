23075cfab18f7f1a218d723acb53f194cf9e0c10
Robert-GainesIII <gaines.robert.edgar@gmail.com>
2019-11-02 21:22:48 -0700

    added indexOF1

---
+++ CS3B/lib/objectFiles/String_indexOf_1.s
@@ -0,0 +1,37 @@
+@*****************************************************************************
+@AUTHOR:		Robert Gaines
+@PROGRAM:   	+String_indexOf_1(string1:String,ch:char):int  
+@[R1]:			points to address of string being searched
+@[R2]:			contains the char to search for
+@
+@RETURNS: 		R0 contains the index of the first occurence, else returns -1
+@
+@*****************************************************************************
+
+.global String_indexOf_1
+
+String_indexOf_1:
+	push {r1-r2, lr}
+
+	mov r4, r1
+	mov r5, r2
+	bl String_length
+	mov r6, r0
+
+indexOf_1Loop:
+	cmp r6, r0
+	beq indexNotFound1
+	ldrb r7, [r4], #1
+	cmp r7, r5
+	beq indexFound1
+	sub r6,#0
+	b indexOf_1Loop
+indexFound1:
+	mov r0, r6
+	b end_indexof_1
+indexNotFound1:
+	mov r0, #-1
+end_indexof_1: 
+	pop {r1-r2, lr}
+	
+	bx lr
