d9a3b31ba647ef650a7f3500ae744a0dd3285f0a
Robert-GainesIII <gaines.robert.edgar@gmail.com>
2019-11-02 23:40:15 -0700

    update

---
+++ CS3B/lib/objectFiles/String_lastIndexOf_2.s
@@ -0,0 +1,39 @@
+@*****************************************************************************
+@AUTHOR:		Robert Gaines
+@PROGRAM:   	+String_indexOf_2(string1:String,ch:char,fromIndex:int):int     
+@[R1]:			points to address of string being searched
+@[R2]:			contains the char to search for
+@[R3]:			contains index to start at
+@
+@RETURNS: 		R0 contains the index of the first occurence, else returns -1
+@
+@*****************************************************************************
+
+.global String_indexOf_2
+
+String_indexOf_2:
+	push {r1-r2, lr}
+
+	mov r5, r2
+	mov r6, r3
+	LDR R1, [R1]
+	mov r4, r1
+	
+
+indexOf_2Loop:
+	cmp r6, #0
+	beq indexNotFound2
+	ldrb r7, [r4,r6]
+	cmp r7, r5
+	beq indexFound2
+	sub r6,#1
+	b indexOf_2Loop
+indexFound2:
+	mov r0, r6
+	b end_indexof_2
+indexNotFound2:
+	mov r0, #-1
+end_indexof_2: 
+	pop {r1-r2, lr}
+	
+	bx lr
