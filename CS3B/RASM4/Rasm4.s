39c6a194e71ca478ad4cca5c260ede33412c3e8f
Robert-GainesIII <gaines.robert.edgar@gmail.com>
2019-11-26 12:39:47 -0800

    almost done adding debug for node count

--- CS3B/RASM4/Rasm4.s
+++ CS3B/RASM4/Rasm4.s
@@ -183,7 +183,6 @@ editGetIndex:
 				
 				LDR R2, =temp
 				LDR R3, [R2]
-				LDR R1, [R3]    //FIRST 4 BYTES OF NODE
 				STR R5, [R1]    //STORE NEW STRING INTO THE DATA SECTION OF THE NODE
 				
 				b endEdit
