d55b080c76103051b45809bcde732d335692b94e
Robert-GainesIII <gaines.robert.edgar@gmail.com>
2019-12-03 15:47:29 -0800

    lab 21 init

--- CS3B/Lab21/main.c
+++ CS3B/Lab21/main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-3:
-4:  main()
-5:  {
+  main()
+  {
 	   int dummyVariable;
 	   char string[] = "I love assembly language and pizza!\0";
-6:     printf("length of the string is: %d", String_length(dummyVariable, string));
-7:     return 0;
-8:  }
+     printf("length of the string is: %d", String_length(dummyVariable, string));
+     return 0;
+  }
