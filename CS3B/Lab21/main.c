52ffdcc9073cfa973fafce81d68befb4ffca7119
Robert-GainesIII <gaines.robert.edgar@gmail.com>
2019-12-03 15:49:02 -0800

    lab 21 init

--- CS3B/Lab21/main.c
+++ CS3B/Lab21/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-  main()
+
+  extern int myadd(int a, char []);
+  
+  int main()
   {
 	   int dummyVariable;
 	   char string[] = "I love assembly language and pizza!\0";
-     printf("length of the string is: %d", String_length(dummyVariable, string));
-     return 0;
+	   printf("length of the string is: %d", String_length(dummyVariable, string));
+	   return 0;
   }
