2e3a40a48718fde5246c8a7cbbdd5cbf0c540694
Robert-GainesIII <gaines.robert.edgar@gmail.com>
2019-12-10 16:01:49 -0800

    RASM5 init commit

--- CS3B/RASM5/RASM5.c
+++ CS3B/RASM5/RASM5.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-  extern int iSortA(int [200000]);
-  extern int iSortC(int [200000]);
+  extern int iSortA(int *);
+  extern int iSortC(int *);
+  int loadFile(int *, int *);
   
   int main()
   {
+	   int dataSetA[200000];
+	   int dataSetC[200000];
 	   int elementCount = 0;
 	   int aTime = 0, cTime = 0;
 	   int choice = 0;
@@ -31,6 +34,30 @@
 			   choice = temp;
 		   }
 		   
+		   
+		   if(choice == 1){
+			  elementCount = loadFile(dataSetA, dataSetC); 
+		   }
+		   
 	   }
 	   return 0;
+  }
+  
+  int loadFile(int *a, int *c){
+	  
+	  /* Pointer to the file */
+  
+	  FILE* file = fopen ("input.txt", "r");
+	  int i = 0;
+      int count = 0;
+	  fscanf (file, "%d", &i);    
+	  while (!feof (file))
+		{  
+		  a[count] = i;
+		  c[count] = i;
+	      count ++;
+		  fscanf (file, "%d", &i);      
+		}
+	  fclose (file);        
+	  return count;
   }
