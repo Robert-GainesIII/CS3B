58ca9bff3e94d5c830e59215147769f970bbd3c6
Robert-GainesIII <gaines.robert.edgar@gmail.com>
2019-12-12 02:32:20 -0800

    updates

--- CS3B/RASM5/RASM5.cpp
+++ CS3B/RASM5/RASM5.cpp
@@ -21,7 +21,7 @@
 	   int x;
 	   char c;
 	   string temp;
-	   ifstream input("input.txt");
+	   ifstream input;
 	   ofstream output1;
 	   ofstream output2;
 	   ofstream output3;
@@ -57,7 +57,8 @@
 		   
 		   switch(choice){
 			   case 1:
-					//input.open("/home/pi/CS3B/CS3B/RASM5/input.txt", ios::in);
+					//input.open("/home/pi/CS3B/CS3B/RASM5/input.txt"
+					input.open("input.txt");
 					if(!input){
 						printf("File was not opened!");
 						break;
