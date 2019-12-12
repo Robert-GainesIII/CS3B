732d801fb9b902541b6a8e0b404660ebd09942c3
Robert-GainesIII <gaines.robert.edgar@gmail.com>
2019-12-12 03:20:04 -0800

    updates

--- CS3B/RASM5/RASM5.cpp
+++ CS3B/RASM5/RASM5.cpp
@@ -67,6 +67,7 @@
 						for(int i =0; i< 200000; i++)
 						{
 							getline(input, str, '\n');
+							if(!input)break;
 							x = stoi(str);
 							//input.ignore(10, '\n');
 							//printf("value at line %d: %d", i, x);
